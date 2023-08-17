ecd78e9433186c193c190bd1725654bfd74c647e
sorawit911 <mybile.pa30@gmail.com>
2023-08-17 19:26:28 +0700

    'page82'

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,29 @@
-let express = require('express')
-const app = express()
+let express = require("express");
+let bodyParser = require("body-parser")
+const {sequelize} = require('./models')
 
-app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to waranya')
-})
+const config = require('./config/config')
 
-app.get('/hello/:name', function(req,res){
-    console.log('hello - ' + req.params.name)
-    res.send('say hello with ' + req.params.name)
-})
+const app = express();
 
-let port = 8081
-app.listen(port, function(){
-    console.log('server runing on ' + port)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./routes')(app)
+
+app.get("/status", function (req, res) {
+  res.send("Hello nodejs server belong to sorawit");
+});
+
+app.get("/hello/:name", function (req, res) {
+  console.log("hello - " + req.params.name);
+  res.send("sey hello eith" + req.params.name);
+});
+
+let port = process.env.PORT || config.port
+
+sequelize.sync({force: false}).then(() => {
+  app.listen(port, function () {
+    console.log("server running on " + port);
+  })
 })
